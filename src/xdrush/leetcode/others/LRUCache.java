651c352784125fa10c8d82d1b8e16ad8237ba19b
xudong <xudong1@xiaomi.com>
2023-01-10 12:41:08 +0800

    Add lru cache

---
+++ src/xdrush/leetcode/others/LRUCache.java
@@ -0,0 +1,55 @@
+package xdrush.leetcode.others;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * @Reference: https://juejin.cn/post/6959192029372874759
+ *
+ * Implemention of LRU cache
+ */
+public class LRUCache {
+
+    static class Node {
+        public int key, val;
+        public Node(int k, int v) {
+            this.key = k;
+            this.val = v;
+        }
+    }
+
+    // LRU容量
+    int capacity;
+    HashMap<Integer, Node> map;
+    LinkedList<Node> cache;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>(capacity);
+        cache = new LinkedList<>();
+    }
+
+    public int get(int key) {
+        if (!this.map.containsKey(key)) {
+            return -1;
+        }
+        Node node = this.map.get(key);
+        // 获取到元素之后，要把获取的元素排到队列的最上面
+        cache.remove(node);
+        cache.addFirst(node);
+        return node.val;
+    }
+    
+    public void put(int key, int val) {
+        Node node = new Node(key, val);
+        if (map.containsKey(key)) {
+            cache.remove(map.get(key));
+        } else if (capacity == map.size()) {
+            Node last = cache.getLast();
+            map.remove(last.key);
+        }
+
+        cache.addFirst(node);
+        map.put(key, node);
+    }
+}
